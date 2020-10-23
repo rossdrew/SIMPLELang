2f9d1f8cc4f9fafad762a8e244f3125a574ff146
rossdrew <ross.drew@symphonicsoft.com>
2020-10-23 12:04:31 +0100

    Expanding on property tests

--- src/test/kotlin/com/rox/expression/KotestProperties.kt
+++ src/test/kotlin/com/rox/expression/KotestProperties.kt
@@ -1,24 +1,42 @@
 package com.rox.expression
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
-import io.kotest.property.RandomSource
-import io.kotest.property.Sample
+import io.kotest.property.arbitrary.bool
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.map
 import io.kotest.property.checkAll
 
 
 class KotestProperties: StringSpec({
-    "Addition" {
-        val genSimpleNumber: Arb<SimpleNumber> = Arb.int().map(::SimpleNumber)
+    val genSimpleNumber: Arb<SimpleNumber> = Arb.int().map(::SimpleNumber)
+    val genSimpleBoolean: Arb<SimpleBoolean> = Arb.bool().map(::SimpleBoolean)
 
+    "Zero friendly numerical operations (Division)" {
         checkAll(500, genSimpleNumber, genSimpleNumber) { a, b ->
-            val result = Add(a, b).reduce() as SimpleNumber
-            val expected = SimpleNumber(a.value + b.value)
-            println("$a + $b = $expected")
-            result shouldBe expected
+            if (a.value != 0 || b.value != 0) {
+                val result = Divide(a, b).reduce() as SimpleNumber
+                val expected = SimpleNumber(a.value / b.value)
+                result shouldBe expected
+            }
         }
     }
+
+    "Zero friendly numerical operations" {
+        checkAll(500, genSimpleNumber, genSimpleNumber) { a, b ->
+            io.kotest.data.forAll(
+                row("Addition: ${a.value}+${b.value}=${a.value+b.value}", Add(a, b), SimpleNumber(a.value+b.value)),
+                row("Multiplication: ${a.value}*${b.value}=${a.value+b.value}", Multiply(a, b), SimpleNumber(a.value*b.value)),
+                row("Subtraction: ${a.value}-${b.value}=${a.value+b.value}", Subtract(a, b),SimpleNumber(a.value-b.value)),
+            ) { _, expression, expectedResult ->
+                val reduction = expression.reduce()
+
+                reduction shouldBe expectedResult
+            }
+        }
+    }
+
+    "Boolean Operations"
 })
